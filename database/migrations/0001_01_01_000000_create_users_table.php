6960767c1228bde3c7ac4e32c8099e4da9961e46
irawanyodi <dykhazta@gmail.com>
2024-12-12 07:42:59 +0700

    create: controller, model, migration - done

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone_number', 13);
+            $table->text('address');
             $table->rememberToken();
             $table->timestamps();
         });
